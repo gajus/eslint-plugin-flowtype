9de667eb443d4871dba33cc1d27e6ce55259f534
Nickolay Maltsev <maltsnikolay@gmail.com>
2016-07-17 08:05:41 +0300

    fix

--- src/utilities/isFlowFileAnnotation.js
+++ src/utilities/isFlowFileAnnotation.js
@@ -4,5 +4,5 @@ export default (comment) => {
     // The flow parser splits comments with the following regex to look for the @flow flag.
     // See https://github.com/facebook/flow/blob/a96249b93541f2f7bfebd8d62085bf7a75de02f2/src/parsing/docblock.ml#L39
     return _.includes(comment.split(/[ \t\r\n\\*/]+/), '@flow') ||
-      _.includes(comment.split(/[ \t\r\n\\*/]+/), '@no-flow')
+      _.includes(comment.split(/[ \t\r\n\\*/]+/), '@no-flow');
 };
