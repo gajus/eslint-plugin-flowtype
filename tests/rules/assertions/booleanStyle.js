7a3a1256a8d01740de12685b18c6d684bd1d6872
Dan Harper <intouch@danharper.me>
2016-09-03 08:42:10 +0100

    feat: new boolean-style rule

---
+++ tests/rules/assertions/booleanStyle.js
@@ -0,0 +1,34 @@
+export default {
+  invalid: [
+    {
+      code: 'type X = bool',
+      errors: [{message: 'Use "boolean", not "bool"'}],
+      output: 'type X = boolean'
+    },
+    {
+      code: 'type X = bool',
+      errors: [{message: 'Use "boolean", not "bool"'}],
+      options: ['boolean'],
+      output: 'type X = boolean'
+    },
+    {
+      code: 'type X = boolean',
+      errors: [{message: 'Use "bool", not "boolean"'}],
+      options: ['bool'],
+      output: 'type X = bool'
+    }
+  ],
+  valid: [
+    {
+      code: 'type X = boolean'
+    },
+    {
+      code: 'type X = boolean',
+      options: ['boolean']
+    },
+    {
+      code: 'type X = bool',
+      options: ['bool']
+    }
+  ]
+};
