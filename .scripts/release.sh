5c3cec7f6d3e2905af5c9feffd5a14a14c469cec
Gajus Kuizinas <gajus@gajus.com>
2016-09-05 10:14:53 +0100

    chore: remove unnecessary --force flag

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -45,7 +45,7 @@ git merge $TRAVIS_COMMIT
 # Generate ./README.md from ./.README/README.md template.
 npm run documentation
 
-git add --force ./README.md
+git add ./README.md
 git diff-index --quiet HEAD ./README.md || git commit --no-verify -m 'docs: update documentation' ./README.md
 
 # 1. bump the package.json version (based on your commit history)
