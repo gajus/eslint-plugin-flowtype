e8882dbadc77ba0328a68a227972ed9352447035
Dan Harper <intouch@danharper.me>
2016-10-24 21:17:42 +0100

    refactor: simplify spacings reporter

--- src/rules/typeColonSpacing/reporter.js
+++ src/rules/typeColonSpacing/reporter.js
@@ -1,7 +1,18 @@
 import {spacingFixers} from '../../utilities';
 
+const getSpaces = (direction, colon, context) => {
+  const sourceCode = context.getSourceCode();
+
+  if (direction === 'before') {
+    return colon.start - sourceCode.getTokenBefore(colon).end;
+  } else {
+    return sourceCode.getTokenAfter(colon).start - colon.end;
+  }
+};
+
 export default (direction, context, {always}) => {
-  const check = ({node, spaces, token, name = '', type = 'type annotation'}) => {
+  return ({colon, node, name = '', type = 'type annotation'}) => {
+    const spaces = getSpaces(direction, colon, context);
     const data = {
       direction,
       name,
@@ -11,45 +22,24 @@ export default (direction, context, {always}) => {
     if (always && spaces > 1) {
       context.report({
         data,
-        fix: spacingFixers.stripSpacesAfter(token, spaces - 1),
+        fix: spacingFixers.stripSpaces(direction, colon, spaces - 1),
         message: 'There must be 1 space {{direction}} {{name}}{{type}} colon.',
         node
       });
     } else if (always && spaces === 0) {
       context.report({
         data,
-        fix: spacingFixers.addSpaceAfter(token),
+        fix: spacingFixers.addSpace(direction, colon),
         message: 'There must be a space {{direction}} {{name}}{{type}} colon.',
         node
       });
     } else if (!always && spaces > 0) {
       context.report({
         data,
-        fix: spacingFixers.stripSpacesAfter(token, spaces),
+        fix: spacingFixers.stripSpaces(direction, colon, spaces),
         message: 'There must be no space {{direction}} {{name}}{{type}} colon.',
         node
       });
     }
-  };
-
-  return ({colon, ...rest}) => {
-    const sourceCode = context.getSourceCode();
-    const tokenBefore = sourceCode.getTokenBefore(colon);
-    const tokenAfter = sourceCode.getTokenAfter(colon);
-    let spaces, token;
-
-    if (direction === 'before') {
-      spaces = colon.start - tokenBefore.end;
-      token = tokenBefore;
-    } else {
-      spaces = tokenAfter.start - colon.end;
-      token = colon;
-    }
-
-    check({
-      spaces,
-      token,
-      ...rest
-    });
   };
 };
