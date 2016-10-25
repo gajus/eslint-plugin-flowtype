6b0a677af5558b8c4d0f6a894e4449f7d95cb229
Andres Suarez <zertosh@gmail.com>
2016-10-25 11:58:31 -0400

    fix: Support more properties in object-type-delimiter
    Fixes object-type-delimiter so that it works with dynamic accessors and callable objects:
    
    ```
    type T = {
      (): void,
      [a: string]: number,
    }
    ```

--- tests/rules/assertions/objectTypeDelimiter.js
+++ tests/rules/assertions/objectTypeDelimiter.js
@@ -13,6 +13,30 @@ export default {
       output: 'type Foo = { a: Foo, b: Bar }'
     },
     {
+      code: 'type Foo = { [a: string]: Foo, [b: string]: Bar }',
+      errors: [{message: 'Prefer semicolons to commas in object and class types'}],
+      options: ['semicolon'],
+      output: 'type Foo = { [a: string]: Foo; [b: string]: Bar }'
+    },
+    {
+      code: 'type Foo = { [a: string]: Foo; [b: string]: Bar }',
+      errors: [{message: 'Prefer commas to semicolons in object and class types'}],
+      options: ['comma'],
+      output: 'type Foo = { [a: string]: Foo, [b: string]: Bar }'
+    },
+    {
+      code: 'type Foo = { (): Foo, (): Bar }',
+      errors: [{message: 'Prefer semicolons to commas in object and class types'}],
+      options: ['semicolon'],
+      output: 'type Foo = { (): Foo; (): Bar }'
+    },
+    {
+      code: 'type Foo = { (): Foo; (): Bar }',
+      errors: [{message: 'Prefer commas to semicolons in object and class types'}],
+      options: ['comma'],
+      output: 'type Foo = { (): Foo, (): Bar }'
+    },
+    {
       code: 'declare class Foo { a: Foo, }',
       errors: [{message: 'Prefer semicolons to commas in object and class types'}],
       options: ['semicolon'],
@@ -23,6 +47,42 @@ export default {
       errors: [{message: 'Prefer commas to semicolons in object and class types'}],
       options: ['comma'],
       output: 'declare class Foo { a: Foo, }'
+    },
+    {
+      code: 'declare class Foo { [a: string]: Foo, }',
+      errors: [{message: 'Prefer semicolons to commas in object and class types'}],
+      options: ['semicolon'],
+      output: 'declare class Foo { [a: string]: Foo; }'
+    },
+    {
+      code: 'declare class Foo { a: Foo; }',
+      errors: [{message: 'Prefer commas to semicolons in object and class types'}],
+      options: ['comma'],
+      output: 'declare class Foo { a: Foo, }'
+    },
+    {
+      code: 'declare class Foo { (): Foo, }',
+      errors: [{message: 'Prefer semicolons to commas in object and class types'}],
+      options: ['semicolon'],
+      output: 'declare class Foo { (): Foo; }'
+    },
+    {
+      code: 'declare class Foo { (): Foo; }',
+      errors: [{message: 'Prefer commas to semicolons in object and class types'}],
+      options: ['comma'],
+      output: 'declare class Foo { (): Foo, }'
+    },
+    {
+      code: 'declare class Foo { static (): Foo, }',
+      errors: [{message: 'Prefer semicolons to commas in object and class types'}],
+      options: ['semicolon'],
+      output: 'declare class Foo { static (): Foo; }'
+    },
+    {
+      code: 'declare class Foo { static (): Foo; }',
+      errors: [{message: 'Prefer commas to semicolons in object and class types'}],
+      options: ['comma'],
+      output: 'declare class Foo { static (): Foo, }'
     }
   ],
   valid: [
@@ -35,7 +95,29 @@ export default {
       options: ['comma']
     },
     {
+      code: 'type Foo = { [a: string]: Foo; [b: string]: Bar }',
+      options: ['semicolon']
+    },
+    {
+      code: 'type Foo = { [a: string]: Foo, [b: string]: Bar }',
+      options: ['comma']
+    },
+    {
+      code: 'type Foo = { (): Foo; (): Bar }',
+      options: ['semicolon']
+    },
+    {
+      code: 'type Foo = { (): Foo, (): Bar }',
+      options: ['comma']
+    },
+    {
       code: 'type Foo = { a: Foo, b: Bar }'
+    },
+    {
+      code: 'type Foo = { [a: string]: Foo, [b: string]: Bar }'
+    },
+    {
+      code: 'type Foo = { (): Foo, (): Bar }'
     },
     {
       code: 'declare class Foo { a: Foo; }',
@@ -43,6 +125,22 @@ export default {
     },
     {
       code: 'declare class Foo { a: Foo, }',
+      options: ['comma']
+    },
+    {
+      code: 'declare class Foo { [a: string]: Foo; }',
+      options: ['semicolon']
+    },
+    {
+      code: 'declare class Foo { [a: string]: Foo, }',
+      options: ['comma']
+    },
+    {
+      code: 'declare class Foo { (): Foo; }',
+      options: ['semicolon']
+    },
+    {
+      code: 'declare class Foo { (): Foo, }',
       options: ['comma']
     }
   ]
