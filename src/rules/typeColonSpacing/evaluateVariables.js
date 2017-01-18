11860dc939ad883684ac424b8880383053b84716
Aaron Harvey <aaron.harvey@fairchildsemi.com>
2017-01-18 11:49:57 -0500

    chore: remove left over code

--- src/rules/typeColonSpacing/evaluateVariables.js
+++ src/rules/typeColonSpacing/evaluateVariables.js
@@ -4,14 +4,6 @@ import {getParameterName, quoteName} from '../../utilities';
 export default (context, report) => {
   const sourceCode = context.getSourceCode();
 
-  const getColon = (node, typeAnnotation) => {
-    if (node.type === 'FunctionTypeParam') {
-      return sourceCode.getFirstToken(node, node.optional ? 2 : 1);
-    } else {
-      return sourceCode.getFirstToken(typeAnnotation);
-    }
-  };
-
   return (node) => {
     const declarations = _.get(node, 'declarations', []);
 
@@ -20,7 +12,7 @@ export default (context, report) => {
 
       if (typeAnnotation) {
         report({
-          colon: getColon(leaf, typeAnnotation),
+          colon: sourceCode.getFirstToken(typeAnnotation),
           name: quoteName(getParameterName(leaf, context)),
           node: leaf,
           type: node.kind + ' type annotation'
