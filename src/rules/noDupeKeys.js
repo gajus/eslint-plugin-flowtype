636ceada70418e57d50330d6fcdcac13ddab1745
Paul Brousseau <object88@gmail.com>
2017-03-21 09:41:08 -0600

    fix: no-dupe-keys with legal duplicated keys (#148)

--- src/rules/noDupeKeys.js
+++ src/rules/noDupeKeys.js
@@ -12,13 +12,66 @@ export default (context) => {
     });
   };
 
+  const analizeElement = (element) => {
+    const {type} = element;
+    let value;
+
+    switch (type) {
+    case 'GenericTypeAnnotation':
+      value = element.id.name;
+      break;
+    case 'ObjectTypeAnnotation':
+      // eslint-disable-next-line no-use-before-define
+      value = builObjectStructure(element.properties);
+      break;
+    case 'TupleTypeAnnotation':
+      // eslint-disable-next-line no-use-before-define
+      value = buildArrayStructure(element.types);
+      break;
+    default:
+      value = element.value;
+      break;
+    }
+
+    return {
+      type,
+      value
+    };
+  };
+
+  const buildArrayStructure = (elements) => {
+    return _.map(elements, (element) => {
+      return analizeElement(element);
+    });
+  };
+
+  const builObjectStructure = (properties) => {
+    return _.map(properties, (property) => {
+      const element = analizeElement(property.value);
+
+      return Object.assign(element, {
+        name: getParameterName(property, context)
+      });
+    });
+  };
+
   const checkForDuplicates = (node) => {
     const haystack = [];
 
     _.forEach(node.properties, (identifierNode) => {
-      const needle = getParameterName(identifierNode, context);
+      const needle = {name: getParameterName(identifierNode, context)};
 
-      if (_.includes(haystack, needle)) {
+      if (identifierNode.value.type === 'FunctionTypeAnnotation') {
+        needle.args = _.map(identifierNode.value.params, (param) => {
+          return analizeElement(param.typeAnnotation);
+        });
+      }
+
+      const match = _.some(haystack, (existingNeedle) => {
+        return _.isEqual(existingNeedle, needle);
+      });
+
+      if (match) {
         report(identifierNode);
       } else {
         haystack.push(needle);
