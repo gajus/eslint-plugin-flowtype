768e7910b68eea8dbc6129862d6e6fbc0ebe03c6
Mike Craig <mike@mkscrg.net>
2017-09-16 09:57:23 -0700

    feat: add rule no-unused-expressions
    This rule extends the ESLint's no-unused-expressions to ignore type cast
    expressions.
    
    ESLint's no-unused-expressions catches some likely mistakes, but it also
    complains about bare type cast expressions. These are unlikely to be
    mistakes and are useful in several situations.

---
+++ src/rules/noUnusedExpressions.js
@@ -0,0 +1,23 @@
+// A wrapper around ESLint's core rule no-unused-expressions, additionally ignores type cast
+// expressions.
+
+import coreNOE from 'eslint/lib/rules/no-unused-expressions';
+
+const meta = coreNOE.meta;
+
+const create = (context) => {
+  const coreChecks = coreNOE.create(context);
+
+  return {
+    ExpressionStatement (node) {
+      if (node.expression.type !== 'TypeCastExpression') {
+        coreChecks.ExpressionStatement(node);
+      }
+    }
+  };
+};
+
+export default {
+  create,
+  meta
+};
