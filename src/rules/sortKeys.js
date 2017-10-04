653f960cff49d28e2d02569e8e6469aa61af1a2c
David Buchan-Swanson <david.buchanswanson@gmail.com>
2017-10-05 08:45:36 +1000

    fix: sortKeys auto-fix works with property variance

--- src/rules/sortKeys.js
+++ src/rules/sortKeys.js
@@ -65,12 +65,17 @@ const isValidOrders = {
   }
 };
 
+const variances = {
+  minus: '-',
+  plus: '+'
+};
+
 const generateOrderedList = (context, sort, properties) => {
   return properties.map((property) => {
     const name = getParameterName(property, context);
     const value = context.getSourceCode().getText(property.value);
 
-    return [name + (property.optional ? '?' : ''), value];
+    return [(variances[property.variance] || '') + name + (property.optional ? '?' : ''), value];
   })
     .sort((first, second) => { return sort(first[0], second[0]) ? -1 : 1; })
     .map((item) => { return item[0] + ': ' + item[1]; });
