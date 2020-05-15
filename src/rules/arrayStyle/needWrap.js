68f23955403b6d4e8c76bf84988e15ead763f92a
Gajus Kuizinas <gajus@gajus.com>
2020-05-15 16:05:13 -0700

    style: one element per line

--- src/rules/arrayStyle/needWrap.js
+++ src/rules/arrayStyle/needWrap.js
@@ -1,6 +1,9 @@
 import isSimpleType from './isSimpleType';
 
-const complexTypesWithoutWrap = ['TupleTypeAnnotation', 'ObjectTypeAnnotation'];
+const complexTypesWithoutWrap = [
+  'TupleTypeAnnotation',
+  'ObjectTypeAnnotation',
+];
 
 export default (node) => {
   return !isSimpleType(node) && !complexTypesWithoutWrap.includes(node.type);
