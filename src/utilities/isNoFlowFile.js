accc18bc6b2ed962fe20fb5a9dc7920a242ec102
andrejs.sisojevs <andrejs.sisojevs@accenture.com>
2020-06-29 17:08:05 +0300

    fix: general filter for all rules - should be either @flow file, or `onlyFilesWithFlowAnnotation` should not be enabled and file not marked with @noflow

---
+++ src/utilities/isNoFlowFile.js
@@ -0,0 +1,21 @@
+import isNoFlowFileAnnotation from './isNoFlowFileAnnotation';
+
+/**
+ * Checks whether a file has an @flow or @noflow annotation.
+ *
+ * @param context
+ * @param [strict] - By default, the function returns true if the file starts with @flow but not if it
+ * starts by @noflow. When the strict flag is set to false, the function returns true if the flag has @noflow also.
+ */
+
+export default (context, strict = true) => {
+  const comments = context.getAllComments();
+
+  if (!comments.length) {
+    return false;
+  }
+
+  return comments.some((comment) => {
+    return isNoFlowFileAnnotation(comment.value, strict);
+  });
+};
