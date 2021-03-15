f3b96b7f37862f5d4bde4da1db1b6dae476ab491
Martin Zlámal <mrtnzlml@gmail.com>
2021-03-15 10:50:48 -0600

    docs: regenerate README.md
    Followup after https://github.com/gajus/eslint-plugin-flowtype/commit/fd838c3dce0e5917d7a26ed8074645b2d510cd68
    
    I didn't generate the docs because I misunderstood the contribution guide. This commit fixes it and updates the contribution guide a little bit.

--- tests/rules/assertions/noInternalFlowType.js
+++ tests/rules/assertions/noInternalFlowType.js
@@ -3,69 +3,95 @@ export default {
     {
       code: 'type X = React$AbstractComponent<Config, Instance>',
       errors: [
-        'Type identifier \'React$AbstractComponent\' is not allowed. Use \'React.AbstractComponent\' instead.',
+        {
+          message: 'Type identifier \'React$AbstractComponent\' is not allowed. Use \'React.AbstractComponent\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$ChildrenArray<string>',
       errors: [
-        'Type identifier \'React$ChildrenArray\' is not allowed. Use \'React.ChildrenArray\' instead.',
+        {
+          message: 'Type identifier \'React$ChildrenArray\' is not allowed. Use \'React.ChildrenArray\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$ComponentType<Props>',
       errors: [
-        'Type identifier \'React$ComponentType\' is not allowed. Use \'React.ComponentType\' instead.',
+        {
+          message: 'Type identifier \'React$ComponentType\' is not allowed. Use \'React.ComponentType\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$Config<Prosp, DefaultProps>',
-      errors: ['Type identifier \'React$Config\' is not allowed. Use \'React.Config\' instead.'],
+      errors: [{
+        message: 'Type identifier \'React$Config\' is not allowed. Use \'React.Config\' instead.',
+      }],
     },
     {
       code: 'type X = React$Element<typeof Component>',
-      errors: ['Type identifier \'React$Element\' is not allowed. Use \'React.Element\' instead.'],
+      errors: [{
+        message: 'Type identifier \'React$Element\' is not allowed. Use \'React.Element\' instead.',
+      }],
     },
     {
       code: 'type X = React$ElementConfig<typeof Component>',
       errors: [
-        'Type identifier \'React$ElementConfig\' is not allowed. Use \'React.ElementConfig\' instead.',
+        {
+          message: 'Type identifier \'React$ElementConfig\' is not allowed. Use \'React.ElementConfig\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$ElementProps<typeof Component>',
       errors: [
-        'Type identifier \'React$ElementProps\' is not allowed. Use \'React.ElementProps\' instead.',
+        {
+          message: 'Type identifier \'React$ElementProps\' is not allowed. Use \'React.ElementProps\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$ElementRef<typeof Component>',
       errors: [
-        'Type identifier \'React$ElementRef\' is not allowed. Use \'React.ElementRef\' instead.',
+        {
+          message: 'Type identifier \'React$ElementRef\' is not allowed. Use \'React.ElementRef\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$ElementType',
       errors: [
-        'Type identifier \'React$ElementType\' is not allowed. Use \'React.ElementType\' instead.',
+        {
+          message: 'Type identifier \'React$ElementType\' is not allowed. Use \'React.ElementType\' instead.',
+        },
       ],
     },
     {
       code: 'type X = React$Key',
-      errors: ['Type identifier \'React$Key\' is not allowed. Use \'React.Key\' instead.'],
+      errors: [{
+        message: 'Type identifier \'React$Key\' is not allowed. Use \'React.Key\' instead.',
+      }],
     },
     {
       code: 'type X = React$Node',
-      errors: ['Type identifier \'React$Node\' is not allowed. Use \'React.Node\' instead.'],
+      errors: [{
+        message: 'Type identifier \'React$Node\' is not allowed. Use \'React.Node\' instead.',
+      }],
     },
     {
       code: 'type X = React$Ref<typeof Component>',
-      errors: ['Type identifier \'React$Ref\' is not allowed. Use \'React.Ref\' instead.'],
+      errors: [{
+        message: 'Type identifier \'React$Ref\' is not allowed. Use \'React.Ref\' instead.',
+      }],
     },
     {
       code: 'type X = React$StatelessFunctionalComponent<Props>',
       errors: [
-        'Type identifier \'React$StatelessFunctionalComponent\' is not allowed. Use \'React.StatelessFunctionalComponent\' instead.',
+        {
+          message: 'Type identifier \'React$StatelessFunctionalComponent\' is not allowed. Use \'React.StatelessFunctionalComponent\' instead.',
+        },
       ],
     },
   ],
